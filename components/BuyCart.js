1cdcaa98f1aa14cba25adc7f98630bd5c4691db7
yash03112000 <yashag@iitk.ac.in>
2021-12-03 19:22:07 +0530

    razorpay done

--- components/BuyCart.js
+++ components/BuyCart.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { View, Text, StyleSheet,Alert } from "react-native";
-import { Button, List } from "react-native-paper";
-import RazorpayCheckout from "react-native-razorpay";
-import { RAZORPAY_KEY } from "../config/devProduction";
-import axiosInstance from "../axios/orgin";
+import React from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { Button, List } from 'react-native-paper';
+import RazorpayCheckout from 'react-native-razorpay';
+import { RAZORPAY_KEY } from '../config/devProduction';
+import axiosInstance from '../axios/orgin';
 import * as SecureStore from 'expo-secure-store';
 
 /**
@@ -11,137 +11,163 @@ import * as SecureStore from 'expo-secure-store';
  * @function CourseDetails
  **/
 export const BuyCart = ({ navigation, data }) => {
-  // console.log("wpcoursemeta", wpCourseMeta);
+	// console.log("wpcoursemeta", wpCourseMeta);
 
-  const buyNowHandler = async () => {
-    console.log("you clicked buynow button");
-    //prepare detail to send razorpay
+	const buyNowHandler = async () => {
+		console.log('you clicked buynow button');
+		//prepare detail to send razorpay
 
-    try{
-      const token = await SecureStore.getItemAsync("token");
-      const res = await axiosInstance.post('/user',{token});
-      // console.log(token)
-      // console.log(res.data)
-      const json = res.data.billing
-      if(json.first_name=="" || json.last_name=="" || json.address_1=="" || json.address_2==""
-      ||json.city==""||json.state==""||json.postcode==""||json.country==""||json.email==""||json.phone==""){
-        // alert('Please Complete Your Profile For Purchase')
-        Alert.alert('Alert', 'Please Complete Your Profile For Purchase', [
-          {
-            text: 'Not Now',
-            onPress: () => console.log('Cancel Pressed'),
-            style: 'cancel',
-          },
-          { text: 'Go To Setting', onPress: () => console.log('OK Pressed') },
-        ]);
-    
-      }else{
-        const detailToRazor = {
-          "payment_method":'bacs',
-          "payment_method_title": "Mobile App Direct Bank Transfer",
-          "customer_id": res.data.id,
-          "billing": {
-            "first_name": json.first_name,
-            "last_name": json.last_name,
-            "address_1": json.address_1,
-            "address_2": json.address_2,
-            "city": json.city,
-            "state": json.state,
-            "postcode": json.postcode,
-            "country": json.country,
-            "email": json.email,
-            "phone": json.phone
-          },
-          "courseItems": [
-              {
-                  "courseId": data.ID,
-                  "name": data.post_name,
-                  "salePrice": data.wpCourseMeta._lp_sale_price
-              }
-          ],
-        };
-        console.log(detailToRazor)
-        console.log('what')
-        const order_raw = await axiosInstance.post("/orders", { detailToRazor,token })
-        const order = order_raw.data
-        console.log(order.order_key)
-        console.log(order.id)
-        var options = {
-          description: 'OOkul Course',
-          image: 'https://i.imgur.com/3g7nmJC.png',
-          currency: 'INR',
-          key: RAZORPAY_KEY,
-          amount: '300',
-          name: order.billing.first_name + ' ' + order.billing.last_name,
-          order_id: order.id,//Replace this with an order_id created using Orders API.
-          prefill: {
-            email: order.billing.email,
-            contact: order.billing.phone,
-            name: order.billing.first_name + ' ' + order.billing.last_name
-          },
-          theme: {color: '#53a20e'}
-        }
-        RazorpayCheckout.open(options).then((data) => {
-          // handle success
-          alert(`Success: ${data.razorpay_payment_id}`);
-        }).catch((error) => {
-          // handle failure
-          console.log(error)
-          alert(`Error: ${error.code} | ${error.description}`);
-        });
-      }
+		try {
+			const token = await SecureStore.getItemAsync('token');
+			const res = await axiosInstance.post('/user', { token });
+			// console.log(token)
+			// console.log(res.data)
+			const json = res.data.billing;
+			if (
+				json.first_name == '' ||
+				json.last_name == '' ||
+				json.address_1 == '' ||
+				json.address_2 == '' ||
+				json.city == '' ||
+				json.state == '' ||
+				json.postcode == '' ||
+				json.country == '' ||
+				json.email == '' ||
+				json.phone == ''
+			) {
+				// alert('Please Complete Your Profile For Purchase')
+				Alert.alert('Alert', 'Please Complete Your Profile For Purchase', [
+					{
+						text: 'Not Now',
+						onPress: () => console.log('Cancel Pressed'),
+						style: 'cancel',
+					},
+					{
+						text: 'Go To Setting',
+						onPress: () => {
+							navigation.navigate('Settings', {
+								screen: 'ProfileScreen',
+							});
+						},
+					},
+				]);
+			} else {
+				const detailToRazor = {
+					payment_method: 'bacs',
+					payment_method_title: 'Mobile App Direct Bank Transfer',
+					customer_id: res.data.id,
+					billing: json,
+					courseItems: [
+						{
+							courseId: data.ID,
+							name: data.post_name,
+							salePrice: data.wpCourseMeta._lp_sale_price,
+						},
+					],
+				};
+				// console.log(detailToRazor);
+				// console.log('what');
+				const order_raw = await axiosInstance.post('/orders', {
+					detailToRazor,
+					token,
+				});
+				const order = order_raw.data;
+				// console.log(order);
+				// console.log(order.id);
+				var options = {
+					description: 'OOkul Course',
+					image: 'https://i.imgur.com/3g7nmJC.png',
+					currency: 'INR',
+					key: RAZORPAY_KEY,
+					amount: (parseInt(order.total, 10) * 100).toString(),
+					name: order.billing.first_name + ' ' + order.billing.last_name,
+					// order_id: order.order_key, //Replace this with an order_id created using Orders API.
+					prefill: {
+						email: order.billing.email,
+						contact: order.billing.phone,
+						name: order.billing.first_name + ' ' + order.billing.last_name,
+					},
+					theme: { color: '#53a20e' },
+				};
+				RazorpayCheckout.open(options)
+					.then(async (data) => {
+						// handle success
+						console.log(order.id);
+						var bod = {
+							status: 'completed',
+							payment_method: 'razorpay',
+							payment_method_title: 'Credit Card/Debit Card/NetBanking',
+							transaction_id: data.razorpay_payment_id,
+							customer_ip_address: '27.5.214.252',
+							customer_user_agent:
+								'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+							created_via: 'Mobile-checkout',
+							customer_note: '',
+							date_completed: Date.now(),
+							date_paid: Date.now(),
+						};
+						var res = await axiosInstance.put(`/orders/${order.id}`, {
+							bod,
+							token,
+						});
+						alert(`Success: ${res.status}`);
+					})
+					.catch((error) => {
+						// handle failure
+						console.log(error);
+						alert(`Error: ${error.code} | ${error.description}`);
+					});
+			}
+		} catch (e) {
+			console.log(e);
+			alert(e);
+		}
+		//
+		// alert(`buyNowHandler Clicked`);
+	};
 
+	// const addToCartHandler = () => {
+	//   // navigation.navigate("Cart", { courseId: "courseId" });
+	// };
 
-    }catch(e){
-      console.log(e)
-      alert(e)
-    }
-    //
-    // alert(`buyNowHandler Clicked`);
-  };
-
-  // const addToCartHandler = () => {
-  //   // navigation.navigate("Cart", { courseId: "courseId" });
-  // };
-
-  return (
-    <View>
-      <View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            alignContent: "center",
-            alignItems: "center",
-            margin: 2,
-            fontSize: 25,
-          }}
-        >
-          <Text style={{ fontSize: 25 }}>Price:</Text>
-          <Text
-            style={{
-              textDecorationLine: "line-through",
-              textDecorationStyle: "solid",
-            }}
-          >
-            {"\u20B9"}
-            {data.wpCourseMeta._lp_price}
-          </Text>
-          <Text style={{ fontSize: 25 }}>
-            {"\u20B9"}
-            {data.wpCourseMeta._lp_sale_price}
-          </Text>
-        </View>
-        <View style={{ margin: 2 }}>
-          <Button
-            onPress={buyNowHandler}
-            mode="contained"
-            style={{ padding: 5 }}
-          >
-            BUY NOW
-          </Button>
-          {/* <View>
+	return (
+		<View>
+			<View>
+				<View
+					style={{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-around',
+						alignContent: 'center',
+						alignItems: 'center',
+						margin: 2,
+						fontSize: 25,
+					}}
+				>
+					<Text style={{ fontSize: 25 }}>Price:</Text>
+					<Text
+						style={{
+							textDecorationLine: 'line-through',
+							textDecorationStyle: 'solid',
+						}}
+					>
+						{'\u20B9'}
+						{data.wpCourseMeta._lp_price}
+					</Text>
+					<Text style={{ fontSize: 25 }}>
+						{'\u20B9'}
+						{data.wpCourseMeta._lp_sale_price}
+					</Text>
+				</View>
+				<View style={{ margin: 2 }}>
+					<Button
+						onPress={buyNowHandler}
+						mode="contained"
+						style={{ padding: 5 }}
+					>
+						BUY NOW
+					</Button>
+					{/* <View>
             <Button
               onPress={addToCartHandler}
               style={{ borderColor: "#2596be", borderWidth: 2, margin: 2 }}
@@ -151,8 +177,8 @@ export const BuyCart = ({ navigation, data }) => {
               ADD to CART
             </Button>
           </View> */}
-        </View>
-      </View>
-    </View>
-  );
+				</View>
+			</View>
+		</View>
+	);
 };
--- screen/ProfileScreen.js
+++ screen/ProfileScreen.js
@@ -1,23 +1,157 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Text, View, Button } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Text, View, Button, ScrollView } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import axiosInstance from '../axios/orgin';
 import * as SecureStore from 'expo-secure-store';
 
-import { loginOutUpdate } from "../reducers/authSlice";
 /**
  * @author
  * @function SettingsScreen
  **/
-export const SettingsScreen = () => {
-  const dispatch = useDispatch();
-  const logoutHandler = () => {
-    SecureStore.deleteItemAsync("token");
-    dispatch(loginOutUpdate());
-  };
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>SettingsScreen!</Text>
-      <Button title="Sign out" onPress={logoutHandler} />
-    </View>
-  );
+export const ProfileScreen = () => {
+	const dispatch = useDispatch();
+	const [form, setForm] = useState({
+		first_name: '',
+		last_name: '',
+		address_1: '',
+		address_2: '',
+		city: '',
+		state: '',
+		postcode: '',
+		country: '',
+		email: '',
+		phone: '',
+	});
+
+	useEffect(async () => {
+		const token = await SecureStore.getItemAsync('token');
+		const res = await axiosInstance.post('/user', { token });
+		const json = res.data.billing;
+		setForm({
+			first_name: json.first_name,
+			last_name: json.last_name,
+			address_1: json.address_1,
+			address_2: json.address_2,
+			city: json.city,
+			state: json.state,
+			postcode: json.postcode,
+			country: json.country,
+			email: json.email,
+			phone: json.phone,
+		});
+	}, []);
+
+	const submit = async () => {
+		try {
+			console.log('clicked');
+			const token = await SecureStore.getItemAsync('token');
+			console.log(form);
+			let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+			if (reg.test(form.email)) {
+				const res = await axiosInstance.put('/user', {
+					token,
+					data: {
+						billing: form,
+					},
+				});
+				console.log(res.data.billing);
+			} else {
+				alert('Enter Correct Email ID');
+			}
+		} catch (e) {
+			console.log(e);
+			alert(e);
+		}
+	};
+
+	return (
+		<ScrollView>
+			{/* <Text>Profile Screen!</Text> */}
+			{/* <Button title="Sign out" onPress={logoutHandler} /> */}
+			<View>
+				<TextInput
+					label="First Name*"
+					value={form.first_name}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['first_name']: text }))
+					}
+				/>
+				<TextInput
+					label="Last Name*"
+					value={form.last_name}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['last_name']: text }))
+					}
+				/>
+				<TextInput
+					label="address_1*"
+					value={form.address_1}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['address_1']: text }))
+					}
+				/>
+				<TextInput
+					label="address_2*"
+					value={form.address_2}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['address_2']: text }))
+					}
+				/>
+				<TextInput
+					label="City*"
+					value={form.city}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['city']: text }))
+					}
+				/>
+				<TextInput
+					label="State*"
+					value={form.state}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['state']: text }))
+					}
+				/>
+				<TextInput
+					label="Postcode*"
+					value={form.postcode}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['postcode']: text }))
+					}
+				/>
+				<TextInput
+					label="Country*"
+					value={form.country}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['country']: text }))
+					}
+				/>
+				<TextInput
+					label="Email*"
+					value={form.email}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['email']: text }))
+					}
+				/>
+				<TextInput
+					label="Phone*"
+					value={form.phone}
+					mode="outlined"
+					onChangeText={(text) =>
+						setForm((prev) => ({ ...prev, ['phone']: text }))
+					}
+				/>
+			</View>
+			<Button title="Submit" onPress={submit} />
+		</ScrollView>
+	);
 };
